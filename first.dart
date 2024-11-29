79efdbb39eaf1875203c44e286fde5e171b97eaa
DHARANISRIRAMAN <146571617+DHARANISRIRAMAN@users.noreply.github.com>
2024-11-29 00:57:58 -0800

    Add files via upload

---
+++ first.dart
@@ -0,0 +1,51 @@
+void main() {
+
+	
+	print("Hello World"); 		
+
+	print("This is my first application");
+
+	print(12 / 4);
+
+	print(false);
+}
+#program2
+void main() {
+    print("Hello World");
+    
+    print("This is my first application");
+    
+    print(4 * 5);
+    
+    print(true);
+    
+  }
+  #program3
+  
+void main(List<String> arguments) {
+
+	// Numbers: int
+	int score = 23;
+	var count = 23;     // It is inferred as integer automatically by compiler
+	int hexValue = 0xEADEBAEE;
+
+	// Numbers: double
+	double percentage = 93.4;
+	var percent = 82.533;
+	double exponents = 1.42e5; 
+
+	// Strings
+	String name = "Henry";
+	var company = "Google";
+
+	// Boolean
+	bool isValid = true;
+	var isAlive = false;
+
+	print(score);
+	print(exponents);
+
+	// NOTE: All data types in Dart are Objects.
+	// Therefore, their initial value is by default 'null'
+}
+  
